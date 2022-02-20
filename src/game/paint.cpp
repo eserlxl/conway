baa344855985198c4559e274275b360bcdcb1188
Eser KUBALI <eserkubali@yahoo.com.tr>
2022-02-20 17:58:09 +0300

    Code fix, language fix.

--- src/game/paint.cpp
+++ src/game/paint.cpp
@@ -20,16 +20,14 @@ void GameWidget::paintGrid(QPainter &p) {
     QColor gridColor = m_masterColor; // color of the grid
     gridColor.setAlpha(10); // must be lighter than main color
     p.setPen(gridColor);
-    double cellWidth =
-            floor(double(width()) /
-                  int(universeSizeX) + 1); // width of the widget / number of cells at one row
-    for (double k = 0; k < width(); k += cellWidth) {
+    double cellWidth = floor(double(width()) / universeSizeX) + 1;
+    double cellHeight = floor(double(height()) / universeSizeY) + 1;
+
+    for (unsigned k = 0; k < width(); k += cellWidth) {
         p.drawLine(int(k), 0, int(k), height());
     }
-    double cellHeight = floor(double(
-            height() /
-            int(universeSizeY)) + 1); // height of the widget / number of cells at one row
-    for (double k = 0; k < height(); k += cellHeight) {
+
+    for (unsigned k = 0; k < height(); k += cellHeight) {
         p.drawLine(0, int(k), width(), int(k));
     }
     p.drawRect(borders);
@@ -48,8 +46,8 @@ void GameWidget::paintUniverse(QPainter &p) {
             }
         }
     }
-    double cellWidth = floor(double(width()) / int(universeSizeX) + 1);
-    double cellHeight = floor(double(height() / int(universeSizeY)) + 1);
+    double cellWidth = floor(double(width()) / universeSizeX) + 1;
+    double cellHeight = floor(double(height()) / universeSizeY) + 1;
     for (unsigned int i = 0; i < universeSizeY; i++) {
         for (unsigned int j = 0; j < universeSizeX; j++) {
             unsigned int index = i * universeSizeX + j;
@@ -64,27 +62,27 @@ void GameWidget::paintUniverse(QPainter &p) {
                 int green = masterColor.green();
                 int blue = masterColor.blue();
 
-                int fark = max_value - min_value;
+                int diff = max_value - min_value;
 
-                if (fark > 0) {
+                if (diff > 0) {
 
                     if (elapsedTime < 5) {
                         neighbourhood(i, j);
                     }
 
-                    int palet = limit(fark, 0, 255);
+                    int palette = limit(diff, 0, 255);
 
-                    double oran = double(value[index] - min_value) / fark;
-                    double oran2 = powerValue[index] / 8. / max_value * 4;
-                    double oran3 = power[index] / 8. * 4;
+                    double ratio1 = double(value[index] - min_value) / diff;
+                    double ratio2 = powerValue[index] / 8. / max_value * 4;
+                    double ratio3 = power[index] / 8. * 4;
 
-                    int r = int(palet * limit(oran, 0., 1.));
-                    int g = int(palet * limit(oran2, 0., 1.));
-                    int b = int(palet * limit(oran3, 0., 1.));
+                    int r = int(palette * limit(ratio1, 0., 1.));
+                    int g = int(palette * limit(ratio2, 0., 1.));
+                    int b = int(palette * limit(ratio3, 0., 1.));
 
-                    red = limit(limit(palet, red, 255) - r, 0, 255);
-                    green = limit(limit(palet, green, 255) - g, 0, 255);
-                    blue = limit(limit(palet, blue, 255) - b, 0, 255);
+                    red = limit(limit(palette, red, 255) - r, 0, 255);
+                    green = limit(limit(palette, green, 255) - g, 0, 255);
+                    blue = limit(limit(palette, blue, 255) - b, 0, 255);
                 }
 
                 QColor color = QColor(red, green, blue);
